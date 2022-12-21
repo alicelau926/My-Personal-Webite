81bbbd3cb515b97d3164cbc8d4ceaa2ffcdfbbc2
alicelau926 <104186325+alicelau926@users.noreply.github.com>
2022-12-21 11:50:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,28 +7,27 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-$(function() {
-    $('a[href*=\\#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+$(function () {
+  $('a[href*=\\#]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+  });
 });
 
-function sendEmail() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let usermessage = document.getElementById("message").value;
-  
-  var contactParams = {
-    from_name: name,
-    from_email: email,
-    message: usermessage,
-  };
+(function () {
+    emailjs.init("bD1ZFHucnev4gHelF");
+})();
 
-  emailjs.send("service_exsycyf", "template_rh9neso", contactParams)
-  .then(function(response) {
-    console.log('SUCCESS!', response.status, response.text);
- }, function(error) {
-    console.log('FAILED...', error);
- });
+function sendmail() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let usermessage = document.getElementById("message").value;
+
+        var contactParams = {
+          from_name: name,
+          from_email: email,
+          message: usermessage,
+        };
+
+        emailjs.send('service_exsycyf', 'template_rh9neso', contactParams).then(function (res) {})
 }
